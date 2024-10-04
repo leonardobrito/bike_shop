9a4d40dad5c5bdf0be5ec72ec02ff0f0a90e36db
leonardobrito <f.leobrito@gmail.com>
2024-10-03 21:17:34 -0300

    Initial commit

---
+++ lib/bike_shop_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule BikeShopWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use BikeShopWeb, :controller` and
+  `use BikeShopWeb, :live_view`.
+  """
+  use BikeShopWeb, :html
+
+  embed_templates "layouts/*"
+end
---
+++ lib/bike_shop_web/controllers/page_html.ex
@@ -0,0 +1,10 @@
+defmodule BikeShopWeb.PageHTML do
+  @moduledoc """
+  This module contains pages rendered by PageController.
+
+  See the `page_html` directory for all templates available.
+  """
+  use BikeShopWeb, :html
+
+  embed_templates "page_html/*"
+end
